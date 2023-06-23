bec22041f4f61773f839504b53e87fbf44d487ad
Dhiraj Yadav <132497879+dhirajyadav07@users.noreply.github.com>
2023-06-23 22:41:16 +0530

    Add files via upload

---
+++ createServer.js
@@ -0,0 +1,6 @@
+
+var http=require('http');
+http.createServer((req,resp)=>{
+    resp.write("<h1>hello krishna please take the all happiness of this world to my love sanchi agrawal</h1>");
+    resp.end();
+}).listen(4500);
